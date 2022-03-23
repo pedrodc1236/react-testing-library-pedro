18eb91d7cb345d3394599f9545f9605091f86bd6
Pedro Tavares <pedrodc1236@gmail.com>
2022-03-22 22:43:45 -0300

    requisito 03 completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente <FavoritePokemons.js />', () => {
+  test('Se exibido na tela "No Favorite pokemon found", qnd n houver pokeFav', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const localStoIdFavPoke = localStorage.getItem('favoritePokemonIds');
+    expect(localStoIdFavPoke).toEqual(null);
+    const notFavPoke = screen.getByText(/no favorite pokemon found/i);
+    expect(notFavPoke).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const fire = screen.getByRole('button', { name: /fire/i });
+    userEvent.click(fire);
+    const pokeName = screen.getByText(/charmander/i);
+    expect(pokeName).toBeInTheDocument();
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/4');
+    const checkboxFav = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkboxFav);
+    const localStoIdFavPoke = localStorage.getItem('favoritePokemonIds');
+    expect(localStoIdFavPoke).toEqual('[4]');
+  });
+});
