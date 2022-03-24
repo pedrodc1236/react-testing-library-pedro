df0f37151cc4bb644603dc55adbf8b928669b0b9
Pedro Tavares <pedrodc1236@gmail.com>
2022-03-23 23:18:36 -0300

    requisito 7 ainda incompleto

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente <PokemonDetails.js />', () => {
+  test('Teste se as informações detalhadas do Pokémon selecionado ta na tela', () => {
+    renderWithRouter(<App />);
+    const moreDetailsPoke = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetailsPoke);
+
+    expect(moreDetailsPoke).not.toBeInTheDocument();
+
+    const pokeDetailsHeading = screen.getByRole('heading', { name: /pikachu details/i });
+    expect(pokeDetailsHeading).toBeInTheDocument();
+
+    const detailsSummary = screen.getByRole('heading', { name: /summary/i });
+    expect(detailsSummary).toBeInTheDocument();
+
+    const pokeDetails = screen.getByText(/this intelligent pokémon roasts hard berries/i);
+    expect(pokeDetails).toBeInTheDocument();
+  });
+
+  test('Teste se existe uma seção com os mapas contendo as localizações', () => {
+    renderWithRouter(<App />);
+    console.log(renderWithRouter());
+    const moreDetailsPoke = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetailsPoke);
+
+    const gameLocationsPoke = screen.getByRole('heading',
+      { name: /game locations of pikachu/i, level: 2 });
+    expect(gameLocationsPoke).toBeInTheDocument();
+  });
+});
